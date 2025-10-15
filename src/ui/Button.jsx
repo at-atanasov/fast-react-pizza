b7c8e5525cfd204d4016e43d1460e23ddf06af1a
at-atanasov <at.atanasov@gmail.com>
2025-10-15 21:48:31 +0300

    Styling the Order Form

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 function Button({ children, disabled, to, type }) {
   const base =
-    "inline-block rounded-full bg-yellow-400  font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed";
+    "inline-block text-sm rounded-full bg-yellow-400  font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed";
 
   const styles = {
     primary: base + " md:px-6 md:py-4 px-4 py-3",
     small: base + " py-2 md:px-5 md:py-2.5 px-4 text-xs",
     secondary:
-      "md:px-6 md:py-3.5 px-4 py-2.5 inline-block rounded-full border-2 border-stone-300 font-semibold tracking-wide text-stone-400 uppercase transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:bg-stone-300 focus:ring focus:ring-stone-200 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed",
+      "text-sm md:px-6 md:py-3.5 px-4 py-2.5 inline-block rounded-full border-2 border-stone-300 font-semibold tracking-wide text-stone-400 uppercase transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:bg-stone-300 focus:ring focus:ring-stone-200 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed",
   };
   if (to)
     return (
