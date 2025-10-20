e8511643705eb3667943111c3161607a70532756
at-atanasov <at.atanasov@gmail.com>
2025-10-20 21:29:49 +0300

    Updating Cart Quantities

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -9,6 +9,7 @@ function Button({ children, disabled, to, type, onClick }) {
     small: base + " py-2 md:px-5 md:py-2.5 px-4 text-xs",
     secondary:
       "cursor-pointer text-sm md:px-6 md:py-3.5 px-4 py-2.5 inline-block rounded-full border-2 border-stone-300 font-semibold tracking-wide text-stone-400 uppercase transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:bg-stone-300 focus:ring focus:ring-stone-200 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed",
+    round: base + " py-1 md:px-3.5 md:py-2 px-2.5 text-sm",
   };
   if (to)
     return (
